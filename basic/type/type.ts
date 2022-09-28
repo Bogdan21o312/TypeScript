1a98fbf5f79fe41d9d4ab3c14069e1982c9d1fa9
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-28 18:36:19 +0300

    Number

--- basic/type/type.ts
+++ basic/type/type.ts
@@ -1,7 +1,12 @@
 //BooleanType====================================================================================================
+
 let isCompleted: boolean = false;
 
 // isCompleted = 'abc' <-- Error
 
 isCompleted = true;
 
+//NumberType====================================================================================================
+
+const a: number = 6
+const b: number = 6.1
