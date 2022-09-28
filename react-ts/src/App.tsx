fd85bd8f0a4b6363952022c94c8b046b3683fc66
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-28 20:33:32 +0300

    Counter component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Title from "./components/Title";
+import Counter from "./components/Counter";
 
 function App() {
     return (
         <div className="App">
             <main className="App-main">
                 <Title title={'Hello from react TSX'}/>
+                <Counter title={'Counter'}/>
             </main>
         </div>
     );
