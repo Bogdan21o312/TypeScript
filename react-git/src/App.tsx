f0f7481160a1202dab863f2ff5f3049580913fb7
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-05 20:46:54 +0300

    Update

--- react-git/src/App.tsx
+++ react-git/src/App.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from "./components/Container";
 import Header from "./components/Header";
 import UserCard from "./components/UserCard";
 import {defaultUser} from "./mock";
+import Search from "./components/Search";
+import {GitHubError, GitHubUser, LocalGitHubUser} from "./@types/user";
+import {isGitHubUser} from "./utils/typeguards";
+import {extractLocalUser} from "./utils/exract-local-user";
 
-function App() {
+const BASE_URL = 'https://api.github.com/users/';
+
+const App = () => {
+    const [user, setUser] = useState<LocalGitHubUser | null>(defaultUser);
+
+    const fetchUser = async (username: string) => {
+        const url = BASE_URL + username;
+
+        const res = await fetch(url);
+        const user = await res.json() as GitHubUser | GitHubError;
+
+        if (isGitHubUser(user)) {
+            setUser(extractLocalUser(user));
+        } else {
+            setUser(null);
+        }
+    }
     return (
         <Container>
             <Header/>
-            <UserCard {...defaultUser}/>
+            <Search hasError={!user} onSubmit={fetchUser}/>
+            {user && (
+                <UserCard
+                    {...user}
+                />
+            )}
         </Container>
     );
 }
--- react-git/src/utils/typeguards.ts
+++ react-git/src/utils/typeguards.ts
@@ -1,3 +1,3 @@
 import {GitHubUser} from "../@types/user";
 
-export const isGithubUser = (user: any): user is GitHubUser => 'id' in user;
+export const isGitHubUser = (user: any): user is GitHubUser => 'id' in user;
