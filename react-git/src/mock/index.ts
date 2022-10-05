a3661331fc9dad91d996d9c97317e8b734e66574
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-05 19:00:03 +0300

    Created defaultUser

---
+++ react-git/src/mock/index.ts
@@ -0,0 +1,13 @@
+import {GitHubUser} from "../@types/user";
+
+export const defaultUser: GitHubUser = {
+  "login": "Bogdan21o312",
+  "id": 99550704,
+  "avatar_url": "https://avatars.githubusercontent.com/u/99550704?v=4",
+  "name": "Bohdan Lutskiv",
+  "blog": "",
+  "public_repos": 21,
+  "followers": 1,
+  "following": 1,
+  "created_at": "2022-02-12T16:12:59Z",
+}
