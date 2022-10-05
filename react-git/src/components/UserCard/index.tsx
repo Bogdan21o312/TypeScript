18b17c0fed87657719a8dd9d7acac97b1ee9cde7
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-05 20:20:09 +0300

    Created UserCard

---
+++ react-git/src/components/UserCard/index.tsx
@@ -0,0 +1,41 @@
+import UserInfo from './UserInfo';
+import {UserTitle} from './UserTitle';
+import styles from './UserCard.module.scss';
+import UserStat from "../UserStat";
+import {LocalGitHubUser} from "../../@types/user";
+
+interface UserCardProps extends LocalGitHubUser {
+}
+
+const UserCard = (props: UserCardProps) => {
+    return (
+        <div className={styles.userCard}>
+            <img
+                src={props.avatar_url}
+                alt={props.login}
+                className={styles.avatar}
+            />
+            <UserTitle
+                created={props.created_at}
+                login={props.login}
+                name={props.name}
+            />
+            <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+                {props.bio || 'This profile has no bio'}
+            </p>
+            <UserStat
+                public_repos={props.public_repos}
+                followers={props.followers}
+                following={props.following}
+            />
+            <UserInfo
+                blog={props.blog}
+                company={props.company}
+                location={props.location}
+                twitter_username={props.twitter_username}
+            />
+        </div>
+    );
+};
+
+export default UserCard
