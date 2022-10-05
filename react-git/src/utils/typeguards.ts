e7e292731e7cd2a0e6a725eea55885966f123d34
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-05 20:43:30 +0300

    Updated

--- react-git/src/utils/typeguards.ts
+++ react-git/src/utils/typeguards.ts
@@ -1,3 +1,3 @@
-import { GithubUser } from 'types';
+import {GitHubUser} from "../@types/user";
 
-export const isGithubUser = (user: any): user is GithubUser => 'id' in user; 
+export const isGithubUser = (user: any): user is GitHubUser => 'id' in user;
