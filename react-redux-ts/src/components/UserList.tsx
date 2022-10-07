e31faf59b30bce6476bc3eae72154ddeb5567917
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-07 12:30:24 +0300

    Created conclusion

--- react-redux-ts/src/components/UserList.tsx
+++ react-redux-ts/src/components/UserList.tsx
@@ -1,12 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {fetchUsers} from "../store/action-creators/user";
+import {useActions} from "../hooks/useActions";
 
 const UserList: FC = () => {
     const {users, loading, error} = useTypedSelector(state => state.user)
+    const {fetchUsers} = useActions()
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <h1>{error}</h1>
+    }
 
     return (
         <div>
-
+            {users.map((user, index) =>
+                <div key={index}>{user.name}</div>
+            )}
         </div>
     );
 };
