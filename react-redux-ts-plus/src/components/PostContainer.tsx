c05f1adc051585347066adf8c684ce1b9c13614f
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-08 22:39:03 +0300

    Updated PostContainer.tsx

--- react-redux-ts-plus/src/components/PostContainer.tsx
+++ react-redux-ts-plus/src/components/PostContainer.tsx
@@ -1,23 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import {postAPI} from "../sevices/PostService";
 import PostItem from "./PostItem";
+import {IPost} from "../models/IPost";
 
 const PostContainer = () => {
-    const [limit, setLimit] = useState(10)
+    const [limit, setLimit] = useState(100)
     const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(limit)
+    const [createPost, {error: createError, isLoading: isCreateLoading}] = postAPI.useCreatePostMutation()
+    const [updatePost, {}] = postAPI.useUpdatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
 
     useEffect(() => {
-        setTimeout(() => {
-            setLimit(2)
-        }, 2000)
+        // setTimeout(() => {
+        //     setLimit(8)
+        // }, 2000)
     }, [])
+
+    const handleCreate = async () => {
+        const title = prompt()
+        await createPost({title, body: title} as IPost)
+    }
+
+    const handleRemove = (post: IPost) => {
+        deletePost(post)
+    }
+
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
+    }
 
     return (
         <div className="post__list">
+            <button onClick={handleCreate}>Add new post</button>
             {isLoading && <h1>Loading...</h1>}
             {error && <h1>Error when loading</h1>}
             {posts && posts.map(post =>
-                <PostItem post={post} key={post.id}/>
+                <PostItem remove={handleRemove} update={handleUpdate } post={post} key={post.id}/>
             )}
         </div>
     );
